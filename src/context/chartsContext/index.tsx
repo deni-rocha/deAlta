2ed88ac861826d7c4ebcd737d432f66c0b56d809
Denilson Rocha <eu.denilsonrocha@gmail.com>
2021-12-22 22:21:36 -0300

    v0.0.6 correcao do context

--- src/context/chartsContext/index.tsx
+++ src/context/chartsContext/index.tsx
@@ -1,18 +1,28 @@
-import React, { createContext } from 'react'
-
-import { getCharts } from '../../api'
+import React, { createContext, useState } from 'react'
+import { ICharts } from '../../types/ICharts'
+import { INITIAL_VALUE } from './initialValue'
 
 type ChartsContextProviderProps = {
   children: React.ReactNode
 }
 
-const DEFAULT_VALUE = getCharts()
+type IDEFAULT_VALUE = {
+  dataCharts: ICharts
+  setDataCharts: React.Dispatch<React.SetStateAction<ICharts>>
+}
 
-const ChartsContext = createContext(DEFAULT_VALUE)
+const DEFAULT_VALUE = {
+  dataCharts: INITIAL_VALUE,
+  setDataCharts: () => {},
+}
+
+const ChartsContext = createContext<IDEFAULT_VALUE>(DEFAULT_VALUE)
 
 const ChartsContextProvider = ({ children }: ChartsContextProviderProps) => {
+  const [dataCharts, setDataCharts] = useState(DEFAULT_VALUE.dataCharts)
+
   return (
-    <ChartsContext.Provider value={DEFAULT_VALUE}>
+    <ChartsContext.Provider value={{ dataCharts, setDataCharts }}>
       {children}
     </ChartsContext.Provider>
   )
