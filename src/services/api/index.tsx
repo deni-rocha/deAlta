f3114f5fce87116be8fef3aa82e60b25db234961
Denilson Rocha <eu.denilsonrocha@gmail.com>
2022-01-21 13:50:02 -0300

    redireciona pra nova api

--- src/services/api/index.tsx
+++ src/services/api/index.tsx
@@ -2,13 +2,13 @@ import axios from 'axios'
 import { ICharts } from '../../types/ICharts'
 
 const api = axios.create({
-  baseURL: 'http://api.deezer.com/',
+  baseURL: 'https://api-deezer.herokuapp.com/',
 })
 
 async function getCharts() {
   const chartsDeezer: ICharts[] = []
 
-  const res = await api.get('chart')
+  const res = await api.get('charts')
   chartsDeezer.push(res.data)
 
   return chartsDeezer[0]
