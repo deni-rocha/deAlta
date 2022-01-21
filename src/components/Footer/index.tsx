853ecc5beb411e2ae2762eb93e1962277d6650f5
Denilson Rocha <eu.denilsonrocha@gmail.com>
2022-01-21 14:10:11 -0300

    add component Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import { Div } from './styled'
+
+const Footer = () => {
+  return (
+    <Div>
+      <a href="/"> mostrar tudo </a>
+    </Div>
+  )
+}
+
+export default Footer
---
+++ src/components/Footer/styled.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Div = styled.div`
+width: inherit;
+height: 60px;
+font-style: italic;
+font-weight: 400;
+display: flex;
+justify-content: center;
+align-items: center;
+  a{
+    text-decoration: none;
+  }
+`
+
+export { Div}
