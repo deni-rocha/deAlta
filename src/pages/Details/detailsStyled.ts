1edd98b5eada456e8c00ef695e613644e81a013d
Denilson Rocha <eu.denilsonrocha@gmail.com>
2022-04-02 07:16:01 -0300

    style for detailsPages

---
+++ src/pages/Details/detailsStyled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Div = styled.div`
+color: ${({ theme }) => theme.titles};
+
+`
+const Title = styled.h2`
+font-size: 24px;
+
+`
+export {Div, Title}
