efb3d96b14896b1f00aa8e831f01b49101086f65
Denilson Rocha <eu.denilsonrocha@gmail.com>
2022-04-07 00:16:22 -0300

    created cardTrack

--- src/pages/Details/detailsStyled.ts
+++ src/pages/Details/detailsStyled.ts
@@ -1,11 +1,43 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 const Div = styled.div`
-color: ${({ theme }) => theme.titles};
-
+  color: ${({ theme }) => theme.titles};
 `
-const Title = styled.h2`
-font-size: 24px;
 
+const CardTrack = styled.div`
+  color: #d6e1df;
+  background-color: #00b569;
+  border-radius: 10px;
+  height: 150px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  padding: 12px 12px 0px;
+  margin-top: 24px;
+  .card-li--div {
+    display: flex;
+    justify-content: space-around;
+
+    &---title {
+      margin-bottom: 12px;
+      margin-left: 4px;
+    }
+
+    &---link {
+      color: #a7dbc7;
+      text-decoration: none;
+      align-self: flex-end;
+      justify-self: flex-end;
+      font-size: 14px;
+      &:hover {
+        color: #00092c;
+      }
+    }
+  }
+  .card-li--div audio::-webkit-media-controls-panel {
+    background-color: #a2d5ab;
+  }
 `
-export {Div, Title}
+
+export { Div, CardTrack }
