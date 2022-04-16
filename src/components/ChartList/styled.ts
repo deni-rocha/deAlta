050a1d3c2d676254011d8295c56f9800be99eea7
Denilson Rocha <eu.denilsonrocha@gmail.com>
2022-04-15 22:13:25 -0300

    fix bug theme color in chartList

--- src/components/ChartList/styled.ts
+++ src/components/ChartList/styled.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 const SectionChart = styled.section`
   margin-bottom: 30px;
@@ -10,28 +10,28 @@ const SectionChart = styled.section`
     height: 150px;
     z-index: 99;
     background: ${({ theme }) => theme.background};
-    opacity: .7;
+    opacity: 0.7;
     cursor: pointer;
   }
-  .arrow-left{
+  .arrow-left {
     left: 0;
     @media screen and (min-width: 600px) {
-        left: auto;
-        right: 80%;
-     }
+      left: auto;
+      right: 80%;
+    }
     @media screen and (min-width: 950px) {
-        right: 82%;
-     }
-  } 
+      right: 82%;
+    }
+  }
   .arrow-right {
     right: 0;
     @media screen and (min-width: 600px) {
       right: auto;
       left: 80%;
-     }
+    }
     @media screen and (min-width: 950px) {
       left: 82%;
-     }
+    }
   }
 `
 const H1ChartName = styled.h2`
@@ -45,35 +45,41 @@ const DivChartListArea = styled.div`
   padding-left: 30px;
 `
 const DivChartList = styled.div`
- transition: all ease .5s;
+  transition: all ease 0.5s;
 `
 const DivChartItem = styled.div`
-display: inline-block;
-width: 150px;
-cursor: pointer;
+  display: inline-block;
+  width: 150px;
+  cursor: pointer;
 
- a {
-   text-decoration: none;
-   color: inherit;
- }
- img {
-   border-radius: 14px;
-   width: 100%;
-   transform: scale(0.9);
-   transition: all ease 0.2s;
- }
- img:hover { 
-   transform: scale(1);
- }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  img {
+    border-radius: 14px;
+    width: 100%;
+    transform: scale(0.9);
+    transition: all ease 0.2s;
+  }
+  img:hover {
+    transform: scale(1);
+  }
 
- p{
-  display: block;
-  width: 100px;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  padding-left: 6px;
- }
-
+  p {
+    display: block;
+    width: 100px;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    padding-left: 6px;
+    color: ${({ theme }) => theme.titles};
+  }
 `
-export { SectionChart, H1ChartName, DivChartListArea, DivChartList, DivChartItem }
+export {
+  SectionChart,
+  H1ChartName,
+  DivChartListArea,
+  DivChartList,
+  DivChartItem,
+}
